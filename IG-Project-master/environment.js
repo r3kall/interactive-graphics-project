a4487fcab90ce357595b582fc19a8e6968d9b681
Matthal <30345817+Matthal@users.noreply.github.com>
2017-11-07 16:10:21 +0100

    Add files via upload
    Added track model

---
+++ IG-Project-master/environment.js
@@ -0,0 +1,32 @@
+/* 
+ * environment.js
+ * 
+ * 
+ */
+
+// TODO
+// createGround - praticamente già fatto
+// createRoad - la più tosta, deve curvare.
+// createGuardRails - recinto intorno al campo
+
+function environment(scene) {
+    return 0;
+}
+
+function createSky(scene) {
+    var skyBoxGeometry = new THREE.CubeGeometry( 10000, 10000, 10000 );
+	var skyBoxMaterial = new THREE.MeshBasicMaterial( { color: 0x00ace6, side: THREE.BackSide } );
+	var skyBox = new THREE.Mesh( skyBoxGeometry, skyBoxMaterial );
+	scene.add(skyBox);
+	scene.fog = new THREE.FogExp2( 0x9999ff, 0.00025 );
+}
+
+function createGround() {}
+
+function createRoad() {
+}
+
+function createGuardRails() {}
+
+
+
